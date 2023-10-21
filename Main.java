085a868bcec5073ca9825821f4f783efa6c79906
wbell1 <106184696+wbell1@users.noreply.github.com>
2023-10-20 22:36:14 -0400

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,43 @@
+package CH5;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+import java.util.Scanner;
+
+//got stuck on this
+public class Main
+{
+
+	public static void main(String[] args)
+	{
+		Scanner inputFile = null;
+		int sum = 0;
+		String regex = ",";
+
+		try
+		{
+			 inputFile = new Scanner(new File("src/AccessFile/Writing/CSVPackage/Book1.csv"));
+			inputFile.useDelimiter(regex);
+			
+			while (inputFile.hasNext())
+			{
+				String numString = inputFile.next();
+				int num = Integer.parseInt(numString);
+				sum = sum + num;
+				System.out.println(numString);
+			}
+		} catch (IOException ex)
+		{
+			ex.printStackTrace();
+			System.out.println("The file couldn't be open");
+		} finally
+		{
+			inputFile.close();
+		}
+
+		System.out.println("The sum is : " + sum);
+
+	}
+
+}
